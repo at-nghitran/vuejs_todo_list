3b984aa357e64a4e757ceb398874ccb1b1a4c184
at-nghitran <nghi.tran@asiantech.vn>
2019-10-07 11:35:45 +0700

    add style and UI

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="todos-header">
+    <input type="text" class="todos-input" placeholder="What needs to be done?"/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Header',
+}
+</script>
